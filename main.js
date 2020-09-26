cac5a3d2d6efefddf127261d9f4accb3d3c6a524
Panshin <panshin.and@gmail.com>
2020-09-26 13:17:13 +0700

    . Устанавливается лимит еды в размере 10.000 еды. Еды выше лимита не копится. Добавляется здание Амбар. Каждый амбар увеличивает лимит еды на 5000. Создаёт 15 рабочих мест.

--- main.js
+++ main.js
@@ -15,14 +15,22 @@ class Building {
 
 let farm = new Building("food", 0.7, 25, 0);
 let bank = new Building("money", 0.25, 20, 0);
+let barn = new Building("foodLimit", 50, 15, 0);
 
-const populationGrowth = 0.02;
+const populationGrowth = 0.02, initialFoodLimit = 10000;
 let population = 100;
 let money = 400;
-let food = 130;
+let food = 130, foodLimit = initialFoodLimit;
 let currentTurn = 0;
-let balanceFood, balanceMoney, balancePopulation = 0;
+let balanceFood, balanceMoney, balancePopulation, balanceFoodLimit = 0;
+let constructionCells = 50;
 
+function freeCells() {
+  return constructionCells - totalBuildings();
+}
+function totalBuildings() {
+  return farm.countBuildings + bank.countBuildings + barn.countBuildings;
+}
 
 function balanceAccrual(){
   balanceMoney = Math.floor(Math.min(population,sumWorkplaces()) * 3 + bank.income());
@@ -30,6 +38,10 @@ function balanceAccrual(){
     balanceMoney += Math.floor(population-sumWorkplaces());
   }
   balanceFood = Math.floor(farm.income() - population);
+  foodLimit = barn.income() + initialFoodLimit;
+  //alert(barn.income());
+  if ((food + balanceFood) > foodLimit)
+    balanceFood = foodLimit - food;
   balancePopulation = Math.floor(population * populationGrowth);
   if(food == 0){
     balancePopulation = 0;
@@ -60,7 +72,7 @@ function buildEfficiency(){
   }
 }
 function sumWorkplaces(){
-  return farm.totalWorkplaces() + bank.totalWorkplaces();
+  return farm.totalWorkplaces() + bank.totalWorkplaces() + barn.totalWorkplaces();
 }
 function updateStat(){
   let td;
@@ -95,11 +107,16 @@ function updateStat(){
     td.innerHTML = food;
     if(balanceFood >= 0){
       td.innerHTML += ' + ';
-      td.innerHTML += balanceFood;
+      td.innerHTML += Math.floor(balanceFood);
     }
     else {
-      td.innerHTML += balanceFood;
+      td.innerHTML += Math.floor(balanceFood);
     }
+    td = document.getElementById('constructionCell');
+    td.innerHTML = constructionCells;
+
+    td = document.getElementById('freeCells');
+    td.innerHTML = freeCells();
 
     td = document.getElementById('workplaces');
     td.innerHTML = sumWorkplaces();
@@ -131,11 +148,17 @@ function  building(build){
     let input = document.getElementById(buildName).value;
     let countBuildings = Number(build.value);
     if (countBuildings >= 1){
-      while(money >= 100 && countBuildings >= 1){
-        eval(buildName).countBuildings++;
-        countBuildings--;
-        money = money - 100;
-      }
+        while(money >= 100 && countBuildings >= 1){
+          if (freeCells() >= 1){
+            eval(buildName).countBuildings++;
+            countBuildings--;
+            money = money - 100;
+          }
+          else{
+            alert("Все ячейки строительства заняты");
+            break;
+          }
+        }
     }
     if (money==0 && Building > 0){
       alert("Вы хотите возвести больше построек, чем можете себе позволить. Постройки возведены на все ваши деньги");
